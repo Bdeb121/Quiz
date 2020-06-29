6d662f409ccd834077d327209349ff3121807d00
Bdeb121 <63992699+Bdeb121@users.noreply.github.com>
2020-06-29 21:35:17 +0530

    Add files via upload

---
+++ dist/js/site new.js
@@ -0,0 +1,395 @@
+
+let questions = [
+    {
+      id: 1,
+      question: "The label of a herbarium sheet does not carry information on   (NEET-II 2016)",
+      answer: "Random Access Memory",
+      options: [
+        "date of collection",
+        "name of collector",
+        "local names",
+        "height of the plant"
+      ]
+    },
+    {
+      id: 2,
+      question: "Biosystematics aims at  (AIPMT 2003)",
+      answer: "Central Processing Unit",
+      options: [
+        "the classification of organisms based on broad morphological characters",
+        "delimiting various taxa of organisms and establishing their relationships",
+        "the classification of organisms based on their evolutionary history and establishing their phylogeny on the totality of various parameters from all fields of studies",
+        "identification and arrangement of organisms on the basis of their cytological characteristics"
+      ]
+    },
+    {
+      id: 3,
+      question: "The term phylum was given by",
+      answer: "Electronic Mail",
+      options: [
+        "Cuvier",
+        "Haeckel",
+        "Theophrastus",
+        "Linnaeus"
+      ]
+    },
+    {
+      id: 4,
+      question: "Sequence of taxonomic categories is?",
+      answer: "Random Access Memory",
+      options: [
+        "class–phylum–tribe–order–family–genus–species",
+        "division–class–family–tribe–order–genus–species",
+        "division–class–order–family–tribe–genus–species.",
+        "phylum–order–class–tribe–family–genus–species"
+      ]
+    },
+    {
+      id: 5,
+      question: "What is the full form of CPU?",
+      answer: "Central Processing Unit",
+      options: [
+        "Central Program Unit",
+        "Central Processing Unit",
+        "Central Preload Unit",
+        "None of these"
+      ]
+    },
+    {
+      id: 6,
+      question: "What is the full form of E-mail",
+      answer: "Electronic Mail",
+      options: [
+        "Electronic Mail",
+        "Electric Mail",
+        "Engine Mail",
+        "None of these"
+      ]
+    },
+    {
+      id: 7,
+      question: "What is the full form of RAM ?",
+      answer: "Random Access Memory",
+      options: [
+        "Random Access Memory",
+        "Randomely Access Memory",
+        "Run Aceapt Memory",
+        "None of these"
+      ]
+    },
+    {
+      id: 8,
+      question: "What is the full form of CPU?",
+      answer: "Central Processing Unit",
+      options: [
+        "Central Program Unit",
+        "Central Processing Unit",
+        "Central Preload Unit",
+        "None of these"
+      ]
+    },
+    {
+      id: 9,
+      question: "What is the full form of E-mail",
+      answer: "Electronic Mail",
+      options: [
+        "Electronic Mail",
+        "Electric Mail",
+        "Engine Mail",
+        "None of these"
+      ]
+    },
+    {
+      id: 10,
+      question: "What is the full form of RAM ?",
+      answer: "Random Access Memory",
+      options: [
+        "Random Access Memory",
+        "Randomely Access Memory",
+        "Run Aceapt Memory",
+        "None of these"
+      ]
+    },
+    {
+      id: 11,
+      question: "What is the full form of CPU?",
+      answer: "Central Processing Unit",
+      options: [
+        "Central Program Unit",
+        "Central Processing Unit",
+        "Central Preload Unit",
+        "None of these"
+      ]
+    },
+    {
+      id: 12,
+      question: "What is the full form of E-mail",
+      answer: "Electronic Mail",
+      options: [
+        "Electronic Mail",
+        "Electric Mail",
+        "Engine Mail",
+        "None of these"
+      ]
+    },
+    {
+      id: 13,
+      question: "What is the full form of RAM ?",
+      answer: "Random Access Memory",
+      options: [
+        "Random Access Memory",
+        "Randomely Access Memory",
+        "Run Aceapt Memory",
+        "None of these"
+      ]
+    },
+    {
+      id: 14,
+      question: "What is the full form of CPU?",
+      answer: "Central Processing Unit",
+      options: [
+        "Central Program Unit",
+        "Central Processing Unit",
+        "Central Preload Unit",
+        "None of these"
+      ]
+    },
+    {
+      id: 15,
+      question: "What is the full form of E-mail",
+      answer: "Electronic Mail",
+      options: [
+        "Electronic Mail",
+        "Electric Mail",
+        "Engine Mail",
+        "None of these"
+      ]
+    },
+    {
+      id: 16,
+      question: "What is the full form of RAM ?",
+      answer: "Random Access Memory",
+      options: [
+        "Random Access Memory",
+        "Randomely Access Memory",
+        "Run Aceapt Memory",
+        "None of these"
+      ]
+    },
+    {
+      id: 17,
+      question: "What is the full form of CPU?",
+      answer: "Central Processing Unit",
+      options: [
+        "Central Program Unit",
+        "Central Processing Unit",
+        "Central Preload Unit",
+        "None of these"
+      ]
+    },
+    {
+      id: 18,
+      question: "What is the full form of E-mail",
+      answer: "Electronic Mail",
+      options: [
+        "Electronic Mail",
+        "Electric Mail",
+        "Engine Mail",
+        "None of these"
+      ]
+    },
+    {
+      id: 19,
+      question: "What is the full form of RAM ?",
+      answer: "Random Access Memory",
+      options: [
+        "Random Access Memory",
+        "Randomely Access Memory",
+        "Run Aceapt Memory",
+        "None of these"
+      ]
+    },
+    {
+      id: 20,
+      question: "What is the full form of CPU?",
+      answer: "Central Processing Unit",
+      options: [
+        "Central Program Unit",
+        "Central Processing Unit",
+        "Central Preload Unit",
+        "None of these"
+      ]
+    },
+    {
+      id: 21,
+      question: "What is the full form of E-mail",
+      answer: "Electronic Mail",
+      options: [
+        "Electronic Mail",
+        "Electric Mail",
+        "Engine Mail",
+        "None of these"
+      ]
+    },
+    {
+      id: 22,
+      question: "What is the full form of RAM ?",
+      answer: "Random Access Memory",
+      options: [
+        "Random Access Memory",
+        "Randomely Access Memory",
+        "Run Aceapt Memory",
+        "None of these"
+      ]
+    },
+    {
+      id: 23,
+      question: "What is the full form of CPU?",
+      answer: "Central Processing Unit",
+      options: [
+        "Central Program Unit",
+        "Central Processing Unit",
+        "Central Preload Unit",
+        "None of these"
+      ]
+    },
+    {
+      id: 24,
+      question: "What is the full form of E-mail",
+      answer: "Electronic Mail",
+      options: [
+        "Electronic Mail",
+        "Electric Mail",
+        "Engine Mail",
+        "None of these"
+      ]
+    },
+    {
+      id: 25,
+      question: "What is the full form of RAM ?",
+      answer: "Random Access Memory",
+      options: [
+        "Random Access Memory",
+        "Randomely Access Memory",
+        "Run Aceapt Memory",
+        "None of these"
+      ]
+    },
+    {
+      id: 26,
+      question: "What is the full form of CPU?",
+      answer: "Central Processing Unit",
+      options: [
+        "Central Program Unit",
+        "Central Processing Unit",
+        "Central Preload Unit",
+        "None of these"
+      ]
+    },
+    {
+      id: 27,
+      question: "What is the full form of E-mail",
+      answer: "Electronic Mail",
+      options: [
+        "Electronic Mail",
+        "Electric Mail",
+        "Engine Mail",
+        "None of these"
+      ]
+    },
+    {
+      id: 28,
+      question: "What is the full form of RAM ?",
+      answer: "Random Access Memory",
+      options: [
+        "Random Access Memory",
+        "Randomely Access Memory",
+        "Run Aceapt Memory",
+        "None of these"
+      ]
+    },
+    {
+      id: 29,
+      question: "What is the full form of CPU?",
+      answer: "Central Processing Unit",
+      options: [
+        "Central Program Unit",
+        "Central Processing Unit",
+        "Central Preload Unit",
+        "None of these"
+      ]
+    },
+    {
+      id: 30,
+      question: "What is the full form of E-mail",
+      answer: "Electronic Mail",
+      options: [
+        "Electronic Mail",
+        "Electric Mail",
+        "Engine Mail",
+        "None of these"
+      ]
+    }
+  ];
+  
+  let question_count = 0;
+  let points = 0;
+  
+  window.onload = function() {
+    show(question_count);
+  
+  };
+  
+  function next() {
+  
+     
+    // if the question is last then redirect to final page
+    if (question_count == questions.length - 1) {
+      sessionStorage.setItem("time", time);
+      clearInterval(mytime);
+      location.href = "end.html";
+    }
+    console.log(question_count);
+  
+    let user_answer = document.querySelector("li.option.active").innerHTML;
+    // check if the answer is right or wrong
+    if (user_answer == questions[question_count].answer) {
+      points += 10;
+      sessionStorage.setItem("points", points);
+    }
+    console.log(points);
+  
+    question_count++;
+    show(question_count);
+  }
+  
+  function show(count) {
+    let question = document.getElementById("questions");
+    let [first, second, third, fourth] = questions[count].options;
+  
+    question.innerHTML = `
+    <h2>Q${count + 1}. ${questions[count].question}</h2>
+     <ul class="option_group">
+    <li class="option">${first}</li>
+    <li class="option">${second}</li>
+    <li class="option">${third}</li>
+    <li class="option">${fourth}</li>
+  </ul> 
+    `;
+    toggleActive();
+  }
+  
+  function toggleActive() {
+    let option = document.querySelectorAll("li.option");
+    for (let i = 0; i < option.length; i++) {
+      option[i].onclick = function() {
+        for (let i = 0; i < option.length; i++) {
+          if (option[i].classList.contains("active")) {
+            option[i].classList.remove("active");
+          }
+        }
+        option[i].classList.add("active");
+      };
+    }
+  }
+  
